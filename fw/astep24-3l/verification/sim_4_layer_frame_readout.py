5c3ab52d954ef2a2588e442b067ad68678764a20
zm4632 <richard.leys@kit.edu>
2025-08-28 13:11:06 +0200

    FW Simulation: improved data readout simulation to check no bytes, even IDLE are lost by spi master

--- fw/astep24-3l/verification/sim_4_layer_frame_readout.py
+++ fw/astep24-3l/verification/sim_4_layer_frame_readout.py
@@ -17,7 +17,21 @@ import vip.astropix3
 import astep24_3l_sim
 
 
-@cocotb.test(timeout_time = 0.8 , timeout_unit = "ms")
+async def print_layers_stats(dut,driver):
+
+    for i in range(3):
+        dut._log.info(f"-- Layer {i} stats")
+        idle = await driver.getLayerStatIDLECounter(i)
+        frames = await driver.getLayerStatFRAMECounter(i)
+        errors = await driver.getLayerWrongLength(i)
+        dut._log.info(f"---> IDLE={idle} bytes")
+        dut._log.info(f"---> Frames={frames}")
+        dut._log.info(f"---> Errors={errors}")
+
+async def totalBytesFromAstropix(driver,layer):
+    return (await driver.getLayerStatIDLECounter(layer)) + (await driver.getLayerStatFRAMECounter(layer))*5
+
+@cocotb.test(timeout_time = 2 , timeout_unit = "ms")
 async def test_layer_0_single_frame_noautoread(dut):
 
     ## Driver, asic, clock+reset
@@ -29,25 +43,28 @@ async def test_layer_0_single_frame_noautoread(dut):
     ## Drive a frame from the ASIC with autoread disabled, it should timeout
     try:
         await with_timeout( asic.generateTestFrame(length = 5),200, 'us')
-        assert True == False
     except:
         pass
 
-   
+
     dut._log.info("Current Readout size after untapped frame: %d",await driver.readoutGetBufferSize())
     assert  await driver.readoutGetBufferSize() == 0
 
     ## Now Restart frame generator with a Readout in parallel
     ## Then Write 10 NULL Bytes, which will be enought to readout the whole frame
     generator = cocotb.start_soon(asic.generateTestFrame(length = 5))
-    await Timer(1,units="us")  
+    await Timer(1,units="us")
     await driver.setLayerConfig(layer = 0, reset = False,hold=False,autoread=False,chipSelect=True,flush=True)
-    await driver.writeLayerBytes( layer = 0 , bytes = [0x00]*10 , flush = True)
+    await driver.writeLayerBytes( layer = 0 , bytes = [0xAB]*10 , flush = True)
     await generator.join()
 
     ## Check That one Frame was seen
     await Timer(50, units="us")
-    assert  await driver.readoutGetBufferSize() == 12
+    assert  await driver.readoutGetBufferSize() == 11
+
+    assert (await totalBytesFromAstropix(driver,0)) == asic.spiSlave.bytesOut , "IDLE + Number of Frame bytes must be equal to number of bytes out of model"
+    await print_layers_stats(dut,driver)
+
     await Timer(50, units="us")
 
 @cocotb.test(timeout_time = 2 , timeout_unit = "ms")
@@ -59,34 +76,78 @@ async def test_layer_0_double_frame_noautoread(dut):
     await Timer(10, units="us")
     driver = await astep24_3l_sim.getDriver(dut)
 
-    ## Drive a frame from the ASIC with autoread disabled, it should timeout
-    try:
-        await with_timeout( asic.generateTestFrame(length = 5,framesCount=2),50, 'us')
-        assert True == False
-    except:
-        pass
 
-   
     dut._log.info("Current Readout size after untapped frame: %d",await driver.readoutGetBufferSize())
     assert  await driver.readoutGetBufferSize() == 0
 
     ## Now Restart frame generator with a Readout in parallel
     ## Then Write 10 NULL Bytes, which will be enought to readout the whole frame
     generator = cocotb.start_soon(asic.generateTestFrame(length = 5,framesCount=2))
-    await Timer(1,units="us")  
+    await Timer(1,units="us")
     await driver.setLayerConfig(layer = 0, reset = False,hold=False,autoread=False,chipSelect=True,flush=True)
     await driver.writeLayerBytes( layer = 0 , bytes = [0x00]*16 , flush = True)
     await generator.join()
 
     ## Check That two Frames were seen
     await Timer(50, units="us")
-    assert  await driver.readoutGetBufferSize() == 24
+    assert  await driver.readoutGetBufferSize() == 11*2
 
     ## Readout and print
-    bytes = await driver.readoutReadBytes(24)
+    bytes = await driver.readoutReadBytes(11*2)
     for b in bytes:
         print(f"B={hex(b)}")
+
+
+
+    await print_layers_stats(dut,driver)
+
+    assert (await totalBytesFromAstropix(driver,0)) == asic.spiSlave.bytesOut , "IDLE + Number of Frame bytes must be equal to number of bytes out of model"
+
     await Timer(50, units="us")
+
+
+
+@cocotb.test(timeout_time = 2 , timeout_unit = "ms")
+async def test_layer_0_spi_stall(dut):
+    """Generate more than 66 bytes + 16 spi buffer frames to make the buffers stall"""
+
+    ## Driver, asic, clock+reset
+    asic = vip.astropix3.Astropix3Model(dut = dut, prefix = "layer_0" , chipID = 1)
+    await vip.cctb.common_clock_reset(dut)
+    await Timer(10, units="us")
+    driver = await astep24_3l_sim.getDriver(dut)
+    await Timer(50, units="us")
+
+    await driver.setLayerConfig(layer = 0, reset = False,hold=False,autoread=False,chipSelect=True,flush=True)
+
+
+    ## Drive enough bytes to output buffer to trigger readout
+    #
+    # readoutLength = await driver.readoutGetBufferSize()
+    framesCount = 25
+    bytesToSendToSPI = int(framesCount*5) + 10
+    generator = cocotb.start_soon(asic.generateTestFrame(length = 5,framesCount=framesCount))
+    totalBytes = 0
+    for i in range(int(bytesToSendToSPI/16)+1):
+        await driver.writeLayerBytes( layer = 0 , bytes = [0xAB]*(15) , flush = True)
+        await Timer(25, units="us")
+        readoutLength = await driver.readoutGetBufferSize()
+        if (readoutLength==66):
+            await driver.readoutReadBytes(32)
+            totalBytes += 32
+
+
+
+    await generator.join()
+
+    totalBytes += await driver.readoutGetBufferSize()
+
+    dut._log.info(f"Number of total bytes={totalBytes},expected={framesCount*11}")
+    assert totalBytes == framesCount*11,"Number of total bytes={totalBytes},expected={framesCount*11}"
+    assert (await totalBytesFromAstropix(driver,0)) == asic.spiSlave.bytesOut , "IDLE + Number of Frame bytes must be equal to number of bytes out of model"
+
+    await Timer(50, units="us")
+
 
 @cocotb.test(timeout_time = 2 , timeout_unit = "ms")
 async def test_layer_0_single_frame_autoread(dut):
@@ -108,16 +169,56 @@ async def test_layer_0_single_frame_autoread(dut):
     ## This method returns when the frame was outputed from the chip spi slave
     await asic.generateTestFrame(length = 5)
 
-    ## Wait a bit and get the bytes from the readout
-    await Timer(50, units="us")
+    ## Wait a bit for autoread to be finished and get the bytes from the readout
+    await Timer(250, units="us")
     readoutLength = await driver.readoutGetBufferSize()
-    
-    ## Length should be 6 Frame bytes (header + 5 payload), +2 Readout Frame Header + 4 Timestamp bytes = 12
-    assert readoutLength == 12
 
+    ## Length should be 6 Frame bytes (header + 5 payload), +2 Readout Frame Header + 4 Timestamp bytes = 12
+    assert readoutLength == 11
+
+    await print_layers_stats(dut,driver)
+    dut._log.info(f"Number of bytes out of Astropix model: {asic.spiSlave.bytesOut}")
+
+    assert (await totalBytesFromAstropix(driver,0)) == asic.spiSlave.bytesOut , "IDLE + Number of Frame bytes must be equal to number of bytes out of model"
     await Timer(50, units="us")
+
 
 @cocotb.test(timeout_time = 2 , timeout_unit = "ms")
+async def test_layer_0_two_differentframes_autoread(dut):
+
+    ## Driver, asic, clock+reset
+    asic = vip.astropix3.Astropix3Model(dut = dut, prefix = "layer_0" , chipID = 1)
+    await vip.cctb.common_clock_reset(dut)
+    await Timer(10, units="us")
+    driver = await astep24_3l_sim.getDriver(dut)
+
+    assert await driver.readoutGetBufferSize() == 0
+
+    ##########
+
+    ## Start the layer, with autoread enabled
+    await driver.setLayerConfig(layer = 0, reset = False, hold = False, autoread = True , flush = True )
+
+    ## Drive a frame from the ASIC
+    ## This method returns when the frame was outputed from the chip spi slave
+    await asic.generateTestFrame(length = 5)
+    await Timer(1, units="us")
+    await asic.generateTestFrame(length = 5) # This should run during the extra runtime of autoread
+
+    ## Wait a bit and get the bytes from the readout
+    await Timer(200, units="us")
+    readoutLength = await driver.readoutGetBufferSize()
+
+    ## Length should be 6 Frame bytes (header + 5 payload), +2 Readout Frame Header + 4 Timestamp bytes = 12
+    assert readoutLength == 2*11
+
+    await print_layers_stats(dut,driver)
+    assert (await totalBytesFromAstropix(driver,0)) == asic.spiSlave.bytesOut , "IDLE + Number of Frame bytes must be equal to number of bytes out of model"
+
+
+    await Timer(50, units="us")
+
+@cocotb.test(timeout_time = 2 , timeout_unit = "ms",skip=True)
 async def test_layer_0_single_frame_autoread_stopduringread(dut):
 
     ## Driver, asic, clock+reset
@@ -137,18 +238,18 @@ async def test_layer_0_single_frame_autoread_stopduringread(dut):
 
     ## Drive a frame from the ASIC
     ## This method returns when the frame was outputed from the chip spi slave
-    await asic.generateTestFrame(length = 4)
+    await asic.generateTestFrame(length = 5)
 
     ## Wait for interrupt to be 1 to generate new frames directly
     await RisingEdge(dut.layer_0_interruptn)
     await Timer(2, units="us")
-    await asic.generateTestFrame(length = 4)
+    await asic.generateTestFrame(length = 5)
 
     ## Wait a bit and generate another frame
     await Timer(20, units="us")
-    await asic.generateTestFrame(length = 4)
-    
-   
+    await asic.generateTestFrame(length = 5)
+
+
     ## Generate a wrong length frame
     await Timer(20, units="us")
     await asic.generateTestFrame(length = 5)
@@ -158,7 +259,7 @@ async def test_layer_0_single_frame_autoread_stopduringread(dut):
     await driver.setLayerConfig(layer = 0, reset = False, hold = True, autoread = False , flush = True )
     #await Timer(50, units="us")
     #readoutLength = await driver.readoutGetBufferSize()
-    
+
     ## Length should be 6 Frame bytes (header + 5 payload), +2 Readout Frame Header + 4 Timestamp bytes = 12
     #assert readoutLength == 12
 
@@ -175,7 +276,7 @@ async def test_3_layers_single_frame(dut):
     asics.append(vip.astropix3.Astropix3Model(dut = dut, prefix = "layer_0" , chipID = 1))
     asics.append(vip.astropix3.Astropix3Model(dut = dut, prefix = "layer_1" , chipID = 2))
     asics.append(vip.astropix3.Astropix3Model(dut = dut, prefix = "layer_2" , chipID = 3))
-    
+
     ## Clock/Reset
     await vip.cctb.common_clock_reset(dut)
     await Timer(10, units="us")
@@ -197,7 +298,7 @@ async def test_3_layers_single_frame(dut):
     readoutLength = await driver.readoutGetBufferSize()
 
     ## Each frame length is 12 (see previous test) -> We should have 3*12
-    assert readoutLength == 3*12
+    assert readoutLength == 3*11
 
     await Timer(10, units="us")
 
@@ -226,6 +327,97 @@ async def test_3_layers_single_frame(dut):
     ## Wait a bit and check the readout size
     await Timer(50, units="us")
     readoutLength = await driver.readoutGetBufferSize()
-    assert readoutLength == 3*12
+    assert readoutLength == 3*11
+
+    await Timer(50, units="us")
+
+
+
+
+
+@cocotb.test(timeout_time = 2, timeout_unit = "ms")
+async def test_3_layers_multiple_frames_parallel(dut):
+    """Send A single frame to all layers after each other"""
+
+
+    ## Create ASIC Models
+    asics = []
+    asics.append(vip.astropix3.Astropix3Model(dut = dut, prefix = "layer_0" , chipID = 1))
+    asics.append(vip.astropix3.Astropix3Model(dut = dut, prefix = "layer_1" , chipID = 2))
+    asics.append(vip.astropix3.Astropix3Model(dut = dut, prefix = "layer_2" , chipID = 3))
+
+    ## Clock/Reset
+    await vip.cctb.common_clock_reset(dut)
+    await Timer(10, units="us")
+    driver = await astep24_3l_sim.getDriver(dut)
+
+    await driver.configureLayersFrameTag(enable=True,flush=True)
+
+    await print_layers_stats(dut,driver)
+
+    #########
+
+    ## Start the layers, with autoread enabled
+    #await driver.setLayerConfig(layer = 0, reset = False, hold = False, autoread = True , flush = False )
+    #await driver.setLayerConfig(layer = 1, reset = False, hold = False, autoread = True , flush = False )
+    #await driver.setLayerConfig(layer = 2, reset = False, hold = False, autoread = True , flush = True )
+
+    ## Generate Frames to all in parallel
+    #for i in range(3):
+    #    await asics[i].generateTestFrame(length = 5)
+    #    await asics[i].generateTestFrame(length = 5)
+
+    ## Wait a bit and get the bytes size
+    #await Timer(200, units="us")
+    #readoutLength = await driver.readoutGetBufferSize()
+
+    ## Each frame length is 12 (see previous test) -> We should have 3*12
+    #await print_layers_stats(dut,driver)
+    #assert readoutLength == 3*11*2 , f"Expected {3*11*2} bytes"
+
+    #await Timer(10, units="us")
+
+    #dut._log.info("Done Frames in sequence")
+
+    #await Timer(50, units="us")
+    #return
+    ###########
+
+    ## Same test but generate the frames in parallel
+    ## First warm reset
+    await vip.cctb.warm_reset(dut)
+    await Timer(50, units="us")
+
+    ## Start the layers, with autoread enabled
+    await driver.setLayerConfig(layer = 0, reset = False, hold = False, autoread = True , flush = False )
+    await driver.setLayerConfig(layer = 1, reset = False, hold = False, autoread = True , flush = False )
+    await driver.setLayerConfig(layer = 2, reset = False, hold = False, autoread = True , flush = True )
+    #await Timer(100, units="us") # Wait after enabling for resets to be done, makes waveform easier
+
+    ## Generate Frames
+    counts = 10
+    tasks = []
+    for i in range(3):
+        tasks.append(cocotb.start_soon(asics[i].generateTestFrame(length = 5,framesCount=counts)))
+
+    Combine(*tasks)
+    dut._log.info("Sequencing tasks done")
+
+    ## Wait a bit and check the readout size
+    totalBytes = 0
+    for i in range(6):
+        await Timer(20, units="us")
+        readoutLength = await driver.readoutGetBufferSize()
+        if (readoutLength==0):
+            break
+        totalBytes += readoutLength
+        await driver.readoutReadBytes(readoutLength)
+
+    await print_layers_stats(dut,driver)
+
+    #assert readoutLength == 3*11*counts , f"Expected {3*11*counts} bytes"
+    assert totalBytes == 3*11*counts , f"Expected {3*11*counts} bytes, got {totalBytes} bytes"
+
+
 
     await Timer(50, units="us")
--- fw/astep24-3l/verification/sim_5_layer_longframe.py
+++ fw/astep24-3l/verification/sim_5_layer_longframe.py
@@ -5,7 +5,7 @@ import sys
 import os
 import os.path
 import logging
-import random 
+import random
 import cocotb
 from cocotb.triggers    import Timer,RisingEdge,Join,Combine,with_timeout
 from cocotb.clock       import Clock
@@ -18,9 +18,21 @@ import vip.astropix3
 import astep24_3l_sim
 
 
+async def print_layers_stats(dut,driver):
+
+    for i in range(3):
+        dut._log.info(f"-- Layer {i} stats")
+        idle = await driver.getLayerStatIDLECounter(i)
+        frames = await driver.getLayerStatFRAMECounter(i)
+        errors = await driver.getLayerWrongLength(i)
+        dut._log.info(f"---> IDLE={idle} bytes")
+        dut._log.info(f"---> Frames={frames}")
+        dut._log.info(f"---> Errors={errors}")
+
+
 async def run_gen_read_test(dut,driver,asic,autoRead:bool,framesCount:int,frameLength:int,pause:bool=False):
 
-    ## Config Layer 
+    ## Config Layer
     await driver.setLayerConfig(layer = 0, reset = False, hold = False, autoread = autoRead , flush = True )
     #await driver.configureLayerSPIFrequency(targetFrequencyHz=400000,flush=True)
 
@@ -28,17 +40,17 @@ async def run_gen_read_test(dut,driver,asic,autoRead:bool,framesCount:int,frameL
         if not autoRead:
             await driver.layersSelectSPI(flush=True)
 
-        #framesCount = 150 
+        #framesCount = 150
         #frameLength=5
-        finalBytesCount = framesCount * (frameLength+1+4+2)
+        finalBytesCount = framesCount * (frameLength+4+2)
 
         dut._log.info(f"-- Start, expected {finalBytesCount} bytes, autoread={autoRead} (frames={framesCount},frame length={frameLength},single fw frame length={frameLength+1+4+2} bytes)")
 
         ## Now Restart frame generator with a Readout in parallel
         ## Then Write 10 NULL Bytes, which will be enought to readout the whole frame
         generator = cocotb.start_soon(asic.generateTestFrame(length = frameLength,framesCount=framesCount,isRandom=True,pause=pause))
-        await Timer(150,units="us")  
-        
+        await Timer(150,units="us")
+
         #await generator.join()
         #await RisingEdge(dut.layer_0_interruptn)
 
@@ -68,7 +80,7 @@ async def run_gen_read_test(dut,driver,asic,autoRead:bool,framesCount:int,frameL
 
 
 
-            # get readoutsize 
+            # get readoutsize
             bufferSize = await driver.readoutGetBufferSize()
 
             ## If not autoread, read the buffer full, otherwise the next writeLayerBytes might write too much and hang if the buffers are full
@@ -80,7 +92,7 @@ async def run_gen_read_test(dut,driver,asic,autoRead:bool,framesCount:int,frameL
                     await Timer(100,units="us")
                 else:
                     readBytes.extend(await driver.readoutReadBytes(bufferSize))
-                    
+
             else:
                 # read a random number of bytes
                 randReadCount = random.randint(int(bufferSize/2),bufferSize)
@@ -112,6 +124,106 @@ async def run_gen_read_test(dut,driver,asic,autoRead:bool,framesCount:int,frameL
     finally:
         if not autoRead:
             await driver.layersDeselectSPI(flush=True)
+
+
+
+async def run_gen_read_test_multi(dut,driver,asics,autoRead:bool,framesCount:int,frameLength:int,pause:bool=False):
+
+    ## Config Layer
+    for i in range(len(asics)):
+        await driver.setLayerConfig(layer = i, reset = False, hold = False, autoread = autoRead , flush = True )
+    #await driver.configureLayerSPIFrequency(targetFrequencyHz=400000,flush=True)
+
+    try:
+        if not autoRead:
+            await driver.layersSelectSPI(flush=True)
+
+        #framesCount = 150
+        #frameLength=5
+        finalBytesCount = framesCount * (frameLength+4+2) * len(asics)
+
+        dut._log.info(f"-- Start, expected {finalBytesCount} bytes, autoread={autoRead} (frames={framesCount},frame length={frameLength},single fw frame length={frameLength+4+2} bytes)")
+
+        ## Now Restart frame generator with a Readout in parallel
+        ## Then Write 10 NULL Bytes, which will be enought to readout the whole frame
+        generators = []
+        for i in range(len(asics)):
+            generators.append(cocotb.start_soon(asics[i].generateTestFrame(length = frameLength,framesCount=framesCount,isRandom=True,pause=pause)))
+        await Timer(150,units="us")
+
+        #await generator.join()
+        #await RisingEdge(dut.layer_0_interruptn)
+
+        ## Readout the frames a bit wild, until there are no bytes anymore
+        finished = False
+        readBytes = []
+        mosiTimeout = 0
+        while not finished:
+
+            ## If not autoread, we need to send some bytes to trigger reading out random.randint(16,24)
+            if not autoRead:
+                for i in range(len(asics)):
+                    await driver.writeLayerBytes(layer=i,bytes=[0x00]*16,flush=True)
+
+
+                await Timer(500,units="ns")
+                for i in range(len(asics)):
+                    mosiTimeout = 20
+                    while mosiTimeout>0 and (await driver.getLayerMOSIBytesCount(layer=i) > 0):
+                        await Timer(500,units="ns")
+                        mosiTimeout -= 1
+
+
+
+
+            # get readoutsize
+            bufferSize = await driver.readoutGetBufferSize()
+
+            ## If not autoread, read the buffer full, otherwise the next writeLayerBytes might write too much and hang if the buffers are full
+            ## If the buffersize is 0 without autoread, it is finished
+            if not autoRead:
+                if bufferSize == 0:
+                    finished = True
+                    # Wait a bit here, since the SPI master might still be active
+                    await Timer(100,units="us")
+                else:
+                    readBytes.extend(await driver.readoutReadBytes(bufferSize))
+
+            else:
+                # read a random number of bytes
+                randReadCount = random.randint(int(bufferSize/2),bufferSize)
+                dut._log.info(f"Reading {randReadCount} bytes from buffer ({bufferSize} bytes available)")
+                readBytes.extend(await driver.readoutReadBytes(randReadCount))
+
+                # if nothing else to read, finish
+                await Timer(random.randint(5,50),units="us")
+                if await driver.readoutGetBufferSize() == 0:
+                    dut._log.info(f"No bytes left to read")
+                    finished = True
+
+            dut._log.info(f"--- (autoread={autoRead}) Read {len(readBytes)}/{finalBytesCount} bytes, {(int(len(readBytes)/finalBytesCount * 100))/100 * 100}%")
+
+        #await generator.join()
+        await Combine(*generators)
+        dut._log.info(f"Read {len(readBytes)} bytes, expected {finalBytesCount} bytes")
+        #if (len(readBytes) != finalBytesCount):
+        #    for b in readBytes:
+        #        dut._log.info(f"{hex(b)}")
+        assert(len(readBytes) == finalBytesCount)
+
+        await print_layers_stats(dut,driver)
+
+        ## Now Decode Check
+        ##########
+        #asic.decodeCheckASTEPFramesStaticLength(readBytes,framesCount,frameLength)
+        for i in range(len(asics)):
+            idleBytes = await driver.getLayerStatIDLECounter(i)
+            dut._log.info(f"-- DONE Read {len(readBytes)} bytes, expected {finalBytesCount} bytes, IDLE Bytes={idleBytes} --")
+
+    finally:
+        if not autoRead:
+            await driver.layersDeselectSPI(flush=True)
+
 
 
 @cocotb.test(timeout_time = 500 , timeout_unit = "ms")
@@ -192,6 +304,46 @@ async def test_layer_0_longframe_longtest(dut):
     await Timer(10, units="us")
     driver = await astep24_3l_sim.getDriver(dut)
 
+    await run_gen_read_test(dut,driver,asic,autoRead=False,framesCount=2,frameLength=4,pause=True)
+    await Timer(250,units="us")
+    await run_gen_read_test(dut,driver,asic,autoRead=False,framesCount=30,frameLength=4,pause=True)
+    await Timer(250,units="us")
+    await run_gen_read_test(dut,driver,asic,autoRead=False,framesCount=150,frameLength=4,pause=True)
+    await Timer(250,units="us")
+    await run_gen_read_test(dut,driver,asic,autoRead=False,framesCount=250,frameLength=4,pause=True)
+    await Timer(250,units="us")
+
+    await run_gen_read_test(dut,driver,asic,autoRead=True,framesCount=2,frameLength=4,pause=True)
+    await Timer(250,units="us")
+    await run_gen_read_test(dut,driver,asic,autoRead=True,framesCount=80,frameLength=4,pause=True)
+    await Timer(250,units="us")
+    await run_gen_read_test(dut,driver,asic,autoRead=True,framesCount=250,frameLength=4,pause=True)
+    await Timer(250,units="us")
+
+    return
+
+
+@cocotb.test(timeout_time = 500 , timeout_unit = "ms")
+async def test_layers_longframe_longtest(dut):
+
+    asics = []
+    for i in range(3):
+        asics.append(vip.astropix3.Astropix3Model(dut = dut, prefix = f"layer_{i}" , chipID = i+1))
+
+    logging.getLogger("cocotb.astep24_3l_top.uart_tx").setLevel(logging.WARN)
+    logging.getLogger("cocotb.astep24_3l_top.uart_rx").setLevel(logging.WARN)
+    logging.getLogger("vip.spi").setLevel(logging.WARN)
+
+    ## Reset and driver
+    await vip.cctb.common_clock_reset(dut)
+    await Timer(10, units="us")
+    driver = await astep24_3l_sim.getDriver(dut)
+
+    await run_gen_read_test_multi(dut,driver,asics,autoRead=True,framesCount=2,frameLength=5,pause=True)
+    await Timer(250,units="us")
+    await run_gen_read_test_multi(dut,driver,asics,autoRead=True,framesCount=250,frameLength=5,pause=True)
+
+    return
     await run_gen_read_test(dut,driver,asic,autoRead=False,framesCount=2,frameLength=4,pause=True)
     await Timer(250,units="us")
     await run_gen_read_test(dut,driver,asic,autoRead=False,framesCount=30,frameLength=4,pause=True)
