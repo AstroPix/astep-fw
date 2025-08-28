a82f6bb7326778c855fb785f0aedf0b96d12d0b5
Adrien Laviron <adrien.laviron@nasa.gov>
2025-08-28 09:20:55 -0400

    SW: [chore] (cherry-pick) Changed one cmd

--- sw/scripts/commands.py
+++ sw/scripts/commands.py
@@ -10,11 +10,11 @@ class ComsInterpreter:
     Method getBytes: returns current program
     """
     def __init__(self):
-        self.dict = {"NOP":0xc5, "DRO":0xca, "HKD":0xcc, 
+        self.dict = {"NOP":0xc5, "ROD":0xca, "HKD":0xcc, 
                      "LRC":0xd1, "LOC":0xd4, 
                      "PIX":0xd7, "STH":0xdd, 
                      "SIP":0xe2, "SIV":0xe4, "INJ":0xe7, "JNI":0xe8}
-        self.args = {"NOP":0, "DRO":1, "HKD":1, 
+        self.args = {"NOP":0, "ROD":1, "HKD":1, 
                      "LRC":2, "LOC":2, 
                      "PIX":5, "STH":3, 
                      "SIP":5, "SIV":3, "INJ":0, "JNI":0}
@@ -40,8 +40,8 @@ class ComsInterpreter:
             c = data[i]
             if c in self.dict:
                 if len(data) <= i + self.args[c]:
-                    raise ValueError("Not enough arguments after {}: Needed {}".format(\
-                                      c, self.args[c]))
+                    raise ValueError("Not enough arguments after {}: Needed {} but got {}".format(\
+                                      c, self.args[c], len(data)))
                 for j in range(i+1, i+1+self.args[c]):
                     if data[j] in self.dict:
                         raise ValueError("{} is a command, but {} needs {} arguments".format(\
