42be7b266bca9bcc7b5b40d731e12f5128533eeb
gs66c235 <daniel.p.violette@gmail.com>
2026-08-11 13:58:00 -0400

    added a script to read fw id every continuously until a keyboard interrupt stops it

---
+++ sw/scripts/debug/beagle/read_fw_continuous.py
@@ -0,0 +1,38 @@
+
+"""Read Firmware ID via SPI"""
+import asyncio 
+import drivers.boards
+import struct
+import time
+import rfg.io.spi 
+rfg.io.spi.debug()
+#rfg.io.spi.warning()
+
+async def main():
+    print("Hi")
+
+    ## Open CMOD on Beagle
+    boardDriver = drivers.boards.getCMODSPIDriver("/dev/spidev1.0","/dev/gpiochip2",19)
+    await boardDriver.open()
+    while True:
+        try:
+            id      = await boardDriver.readFirmwareID()
+            version = await boardDriver.readFirmwareVersion()
+            
+            print(f"Firmware ID: {hex(id)}")
+            print(f"Firmware Version: {str(version)}")
+            time.sleep(1)
+        except:
+            print("End of loop")
+            break
+
+    # ofile = open("/home/debian/astep-fw/sw/data/readfwid.bin", "wb")
+    # ofile.write(struct.pack("i", id))
+    # ofile.write(struct.pack("i", version))
+    # ofile.close()
+
+    await boardDriver.close()
+
+
+asyncio.run(main())
+
