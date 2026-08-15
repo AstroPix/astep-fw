cf6f49c4235aff3ba2d1f7912b5271e5a32ef994
Adrien Laviron <adrien.laviron@nasa.gov>
2026-08-15 14:11:47 -0400

    SW: [Fix] (red) resetfsw script

--- sw/resetfsw.sh
+++ sw/resetfsw.sh
@@ -1,6 +1,7 @@
 echo "1,1,1" > bookkeeping_new.csv
 echo "0,0,0" > bookkeeping_rts.csv
 echo "0,0,0" > bookkeeping_mfd.csv
-rm -f data/*.bin
-rm -f data/*.log
-rm test.log
+#rm -f data/*.bin
+#rm -f data/*.log
+rm run.log
+rm bookkeeper.log
